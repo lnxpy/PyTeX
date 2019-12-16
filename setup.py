02afd777e74d63391b9944171b639f5be0b8a6c6
Alireza Yahyapour <lnxpylnxpy@gmail.com>
2019-12-16 05:44:02 -0800

    Initial Commit

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(
-  name = 'pytex-paper',         # How you named your package folder (MyLib)
+  name = 'pytex',         # How you named your package folder (MyLib)
   packages = ['pytex', 'pytex.LaTeX', 'pytex.document'],   # Chose the same as "name"
   version = '1.0',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
